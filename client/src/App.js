2880c54e0462e1a64d15689979aadcd515c0f7df
adi <aditya4161@gmail.com>
2022-07-13 21:08:28 +0530

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,45 @@
-import React from 'react'
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { UserContext } from './utils/UserContext';
+import { magic } from './utils/magic';
+import { useEffect, useState } from 'react';
 
-const App = () => {
+import Cookies from 'universal-cookie';
+import AnimatedRoutes from '../src/components/AnimatedRoutes'
+
+function App() {
+  const cookie = new Cookies();
+  const [user, setUser] = useState();
+
+  async function setCredentials(userData) {
+    setUser(userData);
+    if (!window.localStorage.getItem("didToken")) {
+      let newDidToken = await magic.user.getIdToken({ lifespan: 24 * 60 * 60 });
+      window.localStorage.setItem("didToken", newDidToken);
+    }
+  }
+
+  // If isLoggedIn is true, set the UserContext with user data
+  // Otherwise, set it to {user: null}
+  useEffect(() => {
+    setUser({ loading: true });
+    magic.user.isLoggedIn().then((isLoggedIn) => {
+      return isLoggedIn
+        ? magic.user.getMetadata().then((userData) => setUser(userData))
+        : setUser({ user: null });
+    });
+  }, []);
+
   return (
-    <div className='App'>
-      <Router>
-        <AnimatedRoutes />
-      </Router>
+    <div className="App">
+      <UserContext.Provider value={[user, setUser]}>
+        <Router>
+          <AnimatedRoutes />
+        </Router>
+      </UserContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
