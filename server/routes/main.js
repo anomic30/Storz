785ffd408322a5b2fae5a219ac537a1c4e3e6c2f
Anom Chakravorty <63467479+anomic30@users.noreply.github.com>
2022-09-25 02:22:42 +0530

    Revert "Refactoring Api Route"

--- server/routes/main.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get('/', (req,res)=>{
-    res.send('Welcome to Storz API v1.0!');
-})
-
-module.exports = router
--- server/routes/test.js
+++
@@ -1,8 +0,0 @@
-const authMiddleware = require("../middlewares/authMiddleware");
-const router = require("express").Router();
-
-router.post("/test", authMiddleware, (req, res) => {
-    return res.status(200).json("User can use secure APIs");
-})
-
-module.exports = router
