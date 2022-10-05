90384eb321f377153f76ad3c81a047d19dd02a49
ezehlivinus <ezehlivinusezeh@gmail.com>
2022-10-05 14:59:09 +0100

    chore: This configured ESLint and prettier for the server project

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        endOfLine: 'lf'
+      }
+    ]
+  }
+};
