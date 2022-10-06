bb54d7182601ae05b521f041286e9f7cc5c05b93
spykelion <ndilionel6@gmail.com>
2022-10-06 14:22:31 +0100

    enhancement : long date on file-desc

--- client/src/utils/formatDates.js
+++ client/src/utils/formatDates.js
@@ -4,6 +4,14 @@ export const formatDates = (date) => {
     const month = dateObj.toLocaleString('default', { month: 'short' });
     const day = dateObj.getDate();
     const year = dateObj.getFullYear();
+    return `${day} ${month} ${year}`;
+};
+
+export const formatDateAndTime = (date) => {
+    const dateObj = new Date(date);
+    const month = dateObj.toLocaleString('default', { month: 'short' });
+    const day = dateObj.getDate();
+    const year = dateObj.getFullYear();
     const time = dateObj.toLocaleTimeString()
     return `${day} ${month} ${year} - ${time}`;
-};
+}
