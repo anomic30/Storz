7d1a5daefb1902ddc1888cfa34fe2ab3d04aa7fd
ezehlivinus <ezehlivinusezeh@gmail.com>
2022-10-06 12:01:24 +0100

    Refactored: this refactored /routes/auth.js response

--- server/middlewares/error.middleware.js
+++ server/middlewares/error.middleware.js
@@ -1,7 +1,19 @@
 
 // const logger = require('../config/logging.config')
 const logger = require('../config/logging.config')
+const { SDKError, ErrorCode } = require('@magic-sdk/admin');
+const {  } = require('@magic-sdk');
+
 const { NODE_ENV } = process.env
+/**
+This is not used yet
+const handleMagicLinkError = (error) => {
+	// Depending on the error we can decide serialise, show, hide the info from the client
+	if (error instanceof SDKError) {
+    // Handle...
+  }
+}
+*/
 
 const _error = (error, req, res, next) => {
 	error.datetime = `${new Date()}`;
