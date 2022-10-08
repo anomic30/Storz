2f3e8aa62ac4b4fe0e9b79fc43ba003be8a28dbf
Kit-p <pangkit888@gmail.com>
2022-10-08 23:04:22 +0800

    Add points to landing page globe

--- client/src/components/globe/Globe.jsx
+++ client/src/components/globe/Globe.jsx
@@ -4,6 +4,15 @@ import { default as ReactGlobe } from 'react-globe.gl'
 import countries from '../../assets/datasets/ne_110m_admin_0_countries.json'
 
 function Globe() {
+    // data
+    const countriesData = countries.features
+    const labelsData = countriesData.filter(d => d.properties.POP_RANK >= 15)
+
+    // config
+    const showLabelText = false
+    const labelSize = showLabelText ? 1.5 : 0
+    const labelResolution = showLabelText ? 3 : 0
+
     // transparent globe
     const globeMaterial = new MeshBasicMaterial({
         color: '#121916', // should sync with background-color in "src/pages/landing/Landing.css"
@@ -26,12 +35,21 @@ function Globe() {
             backgroundColor={'rgba(0,0,0,0)'}
             globeMaterial={globeMaterial}
             atmosphereAltitude={0.1}
-            hexPolygonsData={countries.features}
+            hexPolygonsData={countriesData}
             hexPolygonAltitude={0.02}
             hexPolygonResolution={2}
             hexPolygonMargin={0.85}
             hexPolygonColor={useCallback(() => '#00ffa8')}
             hexPolygonCurvatureResolution={0}
+            labelsData={labelsData}
+            labelLat={d => d.properties.LABEL_Y}
+            labelLng={d => d.properties.LABEL_X}
+            labelText={d => d.properties.NAME}
+            labelSize={labelSize}
+            labelDotRadius={0.8}
+            labelColor={useCallback(() => '#ffffff')}
+            labelAltitude={0.03}
+            labelResolution={labelResolution}
             waitForGlobeReady={false}
             animateIn={false}
             rendererConfig={rendererConfig}
