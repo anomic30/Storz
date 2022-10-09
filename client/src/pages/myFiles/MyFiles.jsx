7310f5af3e7c36be4fca97c7fe3e0c13178fe95d
ozair-dev <truemail785@gmail.com>
2022-10-09 21:13:01 +0500

    Added filtering and sorting feature for files

--- client/src/pages/myFiles/MyFiles.jsx
+++ client/src/pages/myFiles/MyFiles.jsx
@@ -1,20 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import './MyFiles.css'
-import search_icon from '../../assets/icons/search.png'
-import Axios from 'axios'
-import Tippy from '@tippyjs/react'
-import 'tippy.js/dist/tippy.css'
-import file_icon from '../../assets/icons/file-icons/normal-file.png'
-import spinner from '../../assets/images/loading.svg'
-import eye_icon from '../../assets/icons/eye.png'
-import { useNavigate } from 'react-router-dom'
-import { formatBytes } from '../../utils/formatBytes'
-import { formatDates } from '../../utils/formatDates';
-import { motion } from 'framer-motion'
+import React, { useCallback, useEffect, useState } from "react";
+import "./MyFiles.css";
+import search_icon from "../../assets/icons/search.png";
+import Axios from "axios";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import file_icon from "../../assets/icons/file-icons/normal-file.png";
+import spinner from "../../assets/images/loading.svg";
+import eye_icon from "../../assets/icons/eye.png";
+import { useNavigate } from "react-router-dom";
+import { formatBytes } from "../../utils/formatBytes";
+import { formatDates } from "../../utils/formatDates";
+import { motion } from "framer-motion";
 
-const tableHeaders = [
-  '', 'Name', '', 'Size', 'Date'
-]
+import { BiSort } from "react-icons/bi";
+import { IoFilter } from "react-icons/io5";
+
+const sortFields = [
+  { name: "File Name", value: "file_name" },
+  { name: "Upload Date", value: "file_creationDate" },
+  { name: "File Size", value: "file_size" },
+];
+
+const sortOrders = [
+  { name: "Ascending", value: 1 },
+  { name: "Descending", value: -1 },
+];
+
+const tableHeaders = ["", "Name", "", "Size", "Date"];
 
 function MyFiles() {
   const navigate = useNavigate();
@@ -22,74 +34,281 @@ function MyFiles() {
   const [isLoading, setIsLoading] = useState(true);
   const [owner, setOwner] = useState("");
   const [search, setSearch] = useState("");
+  const [filterOpen, setFilterOpen] = useState(false);
+  const [type, setType] = useState("");
+  const [types, setTypes] = useState([]);
+  const [privateStatus, setPrivateStatus] = useState(true);
+  const [publicStatus, setPublicStatus] = useState(true);
+  const [sortOpen, setSortOpen] = useState(false);
+  const [sortField, setSortField] = useState("file_name");
+  const [sortOrder, setSortOrder] = useState(1);
 
-  const fetchFiles = async () => {
-    try {
-      setIsLoading(true);
-      const res = await Axios.post(`${process.env.REACT_APP_SERVER_URL}/api/user/files`, {}, { headers: { Authorization: 'Bearer ' + window.localStorage.getItem("didToken") } });
-      setOwner(res.data.owner);
-      setFiles(res.data.files);
-      setIsLoading(false);
-    } catch (err) {
-      setIsLoading(false);
-      console.log(err);
+  const fetchFiles = useCallback(
+    async (abortSignal) => {
+      try {
+        setIsLoading(true);
+        const res = await Axios.post(
+          `${process.env.REACT_APP_SERVER_URL}/api/user/files`,
+          {
+            search,
+            types,
+            privateStatus,
+            publicStatus,
+            sortField,
+            sortOrder,
+          },
+          {
+            headers: {
+              Authorization:
+                "Bearer " + window.localStorage.getItem("didToken"),
+            },
+            signal: abortSignal,
+          }
+        );
+        setOwner(res.data.owner);
+        setFiles(res.data.files);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        console.log(err);
+      }
+    },
+    [privateStatus, publicStatus, search, types, sortField, sortOrder]
+  );
+
+  const handleFilterSubmit = (e) => {
+    e.preventDefault();
+    if (type) {
+      setTypes((prev) => [...prev, type]);
+      setType("");
     }
-  }
+  };
+
+  const handleFilterClose = (e) => {
+    if (!e.path.some((elem) => elem.classList?.contains("filter-div"))) {
+      setFilterOpen(false);
+    }
+  };
+
+  const handleSortClose = (e) => {
+    if (!e.path.some((elem) => elem.classList?.contains("sort-div"))) {
+      setSortOpen(false);
+    }
+  };
 
   useEffect(() => {
-    fetchFiles();
-  }, [])
+    window.addEventListener("click", handleFilterClose);
+    window.addEventListener("click", handleSortClose);
+
+    return () => {
+      window.removeEventListener("click", handleFilterClose);
+      window.removeEventListener("click", handleSortClose);
+    };
+  }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    fetchFiles(controller.signal);
+
+    return () => {
+      setFiles([]);
+      controller.abort();
+    };
+  }, [
+    search,
+    types,
+    privateStatus,
+    publicStatus,
+    sortField,
+    sortOrder,
+    fetchFiles,
+  ]);
 
   return (
-    <motion.div className='MyFiles'
+    <motion.div
+      className="MyFiles"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.2 }}>
+      transition={{ duration: 0.2 }}
+    >
       <div>
         <div className="file-header">
-          <p>
-            My Files
-          </p>
-          <div className='file-desc'>View all of your uploaded files here.</div>
+          <p>My Files</p>
+          <div className="file-desc">View all of your uploaded files here.</div>
         </div>
         <div className="search-con">
           <div className="search-inp">
             <img src={search_icon} alt="" />
-            <input type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <div className="filter-div">
+            <button
+              onClick={() => setFilterOpen((p) => !p)}
+              className="filter-button"
+            >
+              <IoFilter /> <span>Filter</span>
+            </button>
+
+            {filterOpen && (
+              <div className="filter-options">
+                <div className="filter-file-type">
+                  <form onSubmit={handleFilterSubmit}>
+                    <input
+                      value={type}
+                      onChange={(e) => setType(e.target.value)}
+                      type="text"
+                      placeholder="Search for file types"
+                    />
+                  </form>
+
+                  {!!types.length && (
+                    <div className="choice-chips">
+                      {types.map((type, idx) => (
+                        <button
+                          onClick={() =>
+                            setTypes((prev) => prev.filter((_, i) => idx !== i))
+                          }
+                          key={idx}
+                        >
+                          {type}
+                        </button>
+                      ))}
+                    </div>
+                  )}
+                </div>
+
+                <div className="filter-file-status">
+                  <label>
+                    <input
+                      checked={privateStatus}
+                      onChange={(e) => setPrivateStatus(e.target.checked)}
+                      type="checkbox"
+                    />
+                    <span>Private</span>
+                  </label>
+                  <label>
+                    <input
+                      checked={publicStatus}
+                      onChange={(e) => setPublicStatus(e.target.checked)}
+                      type="checkbox"
+                    />
+                    <span>Public</span>
+                  </label>
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="sort-div">
+            <button
+              onClick={() => setSortOpen((p) => !p)}
+              className="sort-button"
+            >
+              <BiSort /> <span>Sort</span>
+            </button>
+            {sortOpen && (
+              <div className="sort-options">
+                <div className="sort-fields">
+                  <p>Sort by</p>
+                  {sortFields.map(({ name, value }) => (
+                    <button
+                      key={value}
+                      isactive={sortField === value ? "true" : "false"}
+                      onClick={() => setSortField(value)}
+                    >
+                      {name}
+                    </button>
+                  ))}
+                </div>
+                <div className="sort-orders">
+                  <p>Order by</p>
+                  {sortOrders.map(({ value, name }) => (
+                    <button
+                      key={value}
+                      isactive={sortOrder === value ? "true" : "false"}
+                      onClick={() => setSortOrder(value)}
+                    >
+                      {name}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
         </div>
-        {isLoading ?
-          <div className='spinner-con'>
+        {isLoading ? (
+          <div className="spinner-con">
             <img src={spinner} alt="Loading..." />
-          </div> :
+          </div>
+        ) : (
           <div className="table-con">
             <div className="table-glass">
               <table>
                 <thead>
-                  <tr >
+                  <tr>
                     {tableHeaders.map((val, idx) => {
-                      return <th className='file-row' key={idx}>{val}</th>
+                      return (
+                        <th className="file-row" key={idx}>
+                          {val}
+                        </th>
+                      );
                     })}
                   </tr>
                 </thead>
                 <tbody>
-                  {files.length > 0 && files.filter(val => {
-                    return val.file_name.toLowerCase().includes(search.toLowerCase());
-                  }).map((val, idx) => {
-                    return <tr className='file-row' key={idx} onClick={() => navigate("/app/myFiles/desc", { state: { val, owner } })}>
-                      <td><img src={file_icon} alt="file-marker" /></td>
-                      <td id='first-col'>{val.file_name ? val.file_name : "test.png"}</td>
-                      <td>{val.public ? <Tippy content="Public" placement='left'><img className="visible" src={eye_icon} alt="public" /></Tippy> : ''}</td>
-                      <td id='size-col'>{val.file_size ? formatBytes(val.file_size) : "69"}</td>
-                      <td>{val.file_creationDate ? formatDates(val.file_creationDate) : "2045/08/30"}</td>
-                    </tr>
-                  })}
+                  {files.length > 0 &&
+                    files.map((val, idx) => {
+                      return (
+                        <tr
+                          className="file-row"
+                          key={idx}
+                          onClick={() =>
+                            navigate("/app/myFiles/desc", {
+                              state: { val, owner },
+                            })
+                          }
+                        >
+                          <td>
+                            <img src={file_icon} alt="file-marker" />
+                          </td>
+                          <td id="first-col">
+                            {val.file_name ? val.file_name : "test.png"}
+                          </td>
+                          <td>
+                            {val.public ? (
+                              <Tippy content="Public" placement="left">
+                                <img
+                                  className="visible"
+                                  src={eye_icon}
+                                  alt="public"
+                                />
+                              </Tippy>
+                            ) : (
+                              ""
+                            )}
+                          </td>
+                          <td id="size-col">
+                            {val.file_size ? formatBytes(val.file_size) : "69"}
+                          </td>
+                          <td>
+                            {val.file_creationDate
+                              ? formatDates(val.file_creationDate)
+                              : "2045/08/30"}
+                          </td>
+                        </tr>
+                      );
+                    })}
                 </tbody>
               </table>
             </div>
-          </div>}
+          </div>
+        )}
       </div>
-    </motion.div>)
+    </motion.div>
+  );
 }
 
-export default MyFiles
+export default MyFiles;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,84 +1,141 @@
 const router = require("express").Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-const AppError = require('./../util/appError')
-const User = require('../models/user')
-const { Magic } = require('@magic-sdk/admin');
+const AppError = require("./../util/appError");
+const User = require("../models/user");
+const { Magic } = require("@magic-sdk/admin");
 const magic = new Magic(process.env.MAGIC_SECRET_KEY);
 
-router.post('/api/user/check', async (req, res , next) => {
-    const email = req.body.email;
-    const user = await User.findOne({ email: email });
-    if (user) {
-        return res.status(200).json({
-            message: "user_found"
-        });
-    } else {
-        return next( new AppError("user_not_found" , 200));
-    }
-})
+router.post("/api/user/check", async (req, res, next) => {
+  const email = req.body.email;
+  const user = await User.findOne({ email: email });
+  if (user) {
+    return res.status(200).json({
+      message: "user_found",
+    });
+  } else {
+    return next(new AppError("user_not_found", 200));
+  }
+});
 
+router.post("/api/user/files", authMiddleware, async (req, res, next) => {
+  const metadata = await magic.users.getMetadataByToken(
+    req.headers.authorization.substring(7)
+  );
+  const magic_id = metadata.issuer;
 
-router.post("/api/user/files", authMiddleware, async (req, res , next) => {
-    const metadata = await magic.users.getMetadataByToken(req.headers.authorization.substring(7));
-    const magic_id = metadata.issuer;
-    const user = await User.findOne({ magic_id: magic_id });
-    if (!user) {
-        return next( new AppError("user_not_found" , 400));
-        
-    }
-    const files = user.files;
-    return res.status(200).json({ files: files, owner: user.user_name });
-})
+  const { search, types, privateStatus, publicStatus, sortField, sortOrder } =
+    req.body;
 
-router.patch("/api/user/makePublic/:cid", authMiddleware, async (req, res, next) => {
-    const metadata = await magic.users.getMetadataByToken(req.headers.authorization.substring(7));
+  // matching files based on the search text and file type
+  const fileFilters = {
+    "files.file_name": new RegExp(`.*${search}.*(${types.join("|")})$`, "i"),
+  };
+
+  // matching based on the file status (public or private)
+  if (!privateStatus && publicStatus) fileFilters["files.public"] = true;
+  else if (!publicStatus && privateStatus) fileFilters["files.public"] = false;
+
+  const result = await User.aggregate([
+    {
+      $match: {
+        magic_id,
+      },
+    },
+    {
+      $unwind: "$files",
+    },
+    {
+      $match: fileFilters,
+    },
+    {
+      $sort: {
+        [`files.${sortField}`]: sortOrder,
+      },
+    },
+    {
+      $group: {
+        _id: "$_id",
+        files: {
+          $push: "$files",
+        },
+        user_name: { $first: "$user_name" },
+      },
+    },
+  ]);
+  const user = result[0];
+
+  if (!user) {
+    return next(new AppError("user_not_found", 400));
+  }
+  const files = user.files;
+  return res.status(200).json({ files: files, owner: user.user_name });
+});
+
+router.patch(
+  "/api/user/makePublic/:cid",
+  authMiddleware,
+  async (req, res, next) => {
+    const metadata = await magic.users.getMetadataByToken(
+      req.headers.authorization.substring(7)
+    );
     const magic_id = metadata.issuer;
     const cid = req.params.cid;
     const state = req.body.state;
     if (!magic_id || !cid) {
-        return next( new AppError("Missing required fields" , 400));
-
+      return next(new AppError("Missing required fields", 400));
     }
     try {
-        await User.updateOne({ magic_id: magic_id, files: { $elemMatch: { cid: cid } } }, { $set: { 'files.$.public': state } });
-        return res.status(200).json({ message: "File visibility updated successfully!" });
+      await User.updateOne(
+        { magic_id: magic_id, files: { $elemMatch: { cid: cid } } },
+        { $set: { "files.$.public": state } }
+      );
+      return res
+        .status(200)
+        .json({ message: "File visibility updated successfully!" });
     } catch (err) {
-        // return res.status(500).json({ error: err.message, message: "File visibility update failed!" });
-        return next( new AppError("File visibility update failed!" , 500));
+      // return res.status(500).json({ error: err.message, message: "File visibility update failed!" });
+      return next(new AppError("File visibility update failed!", 500));
     }
-});
+  }
+);
 
-router.patch("/api/user/deleteFile/:cid", authMiddleware, async (req, res, next) => {
-    console.log("Delete route called!")
-    const metadata = await magic.users.getMetadataByToken(req.headers.authorization.substring(7));
+router.patch(
+  "/api/user/deleteFile/:cid",
+  authMiddleware,
+  async (req, res, next) => {
+    console.log("Delete route called!");
+    const metadata = await magic.users.getMetadataByToken(
+      req.headers.authorization.substring(7)
+    );
     const magic_id = metadata.issuer;
     const cid = req.params.cid;
     if (!magic_id || !cid) {
-        return next( new AppError("Missing required fields" , 400));
-
+      return next(new AppError("Missing required fields", 400));
     }
     try {
-        await User.updateOne({ magic_id: magic_id, files: { $elemMatch: { cid: cid } } }, { $pull: { files: { cid: cid } } });
-        return res.status(200).json({ message: "File deleted successfully!" });
+      await User.updateOne(
+        { magic_id: magic_id, files: { $elemMatch: { cid: cid } } },
+        { $pull: { files: { cid: cid } } }
+      );
+      return res.status(200).json({ message: "File deleted successfully!" });
     } catch (err) {
-        // return res.status(500).json({ error: err.message, message: "File deletion failed!" });
-        return next( new AppError( "File deletion failed!" , 500));
+      // return res.status(500).json({ error: err.message, message: "File deletion failed!" });
+      return next(new AppError("File deletion failed!", 500));
     }
-})
+  }
+);
 
 router.get("/api/user/getName/:id", async (req, res, next) => {
-    const magic_id = req.params.id;
-    if (!magic_id) {
-        return next( new AppError("Missing required fields" , 400));
+  const magic_id = req.params.id;
+  if (!magic_id) {
+    return next(new AppError("Missing required fields", 400));
+  }
+  try {
+    const user = await User.findOne({ magic_id: magic_id }, { user_name: 1 });
+    return res.status(200).json(user);
+  } catch (err) {
+    return next(new AppError(err.message, 500));
+  }
+});
 
-    }
-    try {
-        const user = await User.findOne({ magic_id: magic_id }, { user_name: 1 });
-        return res.status(200).json(user);
-    }
-    catch (err) {
-        return next( new AppError(err.message , 500));
-    }
-})
-
-module.exports = router
+module.exports = router;
