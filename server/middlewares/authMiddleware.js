7ba7ccf46f6138f165d56ddd02f85e3e07c08def
ozair-dev <truemail785@gmail.com>
2022-10-09 22:23:15 +0500

    Added filtering and sorting feature for files

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,15 +1,16 @@
 const { Magic } = require('@magic-sdk/admin');
 const AppError = require('../util/appError');
+
 const magic = new Magic(process.env.MAGIC_SECRET_KEY);
 
 const authMiddleware = async (req, res, next) => {
-    try {
-        const didToken = req.headers.authorization.substring(7);
-        await magic.token.validate(didToken);
-        next();
-    } catch (error) {
-        return next( new AppError(error.message , 401));
-    }
-}
+  try {
+    const didToken = req.headers.authorization.substring(7);
+    await magic.token.validate(didToken);
+    next();
+  } catch (error) {
+    return next(new AppError(error.message, 401));
+  }
+};
 
 module.exports = authMiddleware;
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,7 +1,7 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get('/', (req,res)=>{
-    res.send('Welcome to Storz API v1.0!');
-})
+router.get('/', (req, res) => {
+  res.send('Welcome to Storz API v1.0!');
+});
 
-module.exports = router
+module.exports = router;
