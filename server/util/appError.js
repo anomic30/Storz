caaeafe09bf4217fdb252effe5de436dd9c8fef8
Anom Chakravorty <anomchakravorty3008@gmail.com>
2022-10-14 15:03:22 +0530

    formatted code

--- server/util/appError.js
+++ server/util/appError.js
@@ -1,13 +1,11 @@
-class AppError extends Error{
-    constructor(message , statusCode){
-        super(message)
-        this.statusCode = statusCode
-        if(statusCode === 500)
-            this.status = 'error'
-        else
-            this.status = 'fail'
-        Error.captureStackTrace(this , this.constrctor)
-    }
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    if (statusCode === 500) this.status = 'error';
+    else this.status = 'fail';
+    Error.captureStackTrace(this, this.constrctor);
+  }
 }
 
-module.exports = AppError
+module.exports = AppError;
